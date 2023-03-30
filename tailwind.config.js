156bbc7843a05efe9c75d001b21fd5292ed756d7
hh-pd <hiromiti999@gmail.com>
2023-03-31 01:52:25 +0900

    [feature] breezeの追加及び追加に伴った各ファイルの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.ts",
-        "./resources/**/*.tsx",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.tsx',
     ],
+
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [],
-};
+
+    plugins: [require('@tailwindcss/forms')],
+}
