d91279617df861b87e873a160d461e43c16966e3
hh-pd <hiromiti999@gmail.com>
2023-04-06 01:06:29 +0900

    [fix] guestのスタイルを修正

--- resources/js/Layouts/Guest.tsx
+++ resources/js/Layouts/Guest.tsx
@@ -7,12 +7,12 @@ interface Props {
 
 export default function Guest({ children }: Props) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-10 sm:pt-0 bg-gray-100">
+            <div className="text-xl">
                 <Link href="/">My Routine</Link>
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div className="sm:w-full sm:max-w-md w-5/6 mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
         </div>
