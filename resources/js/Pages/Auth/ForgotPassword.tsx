2264bdeb846f85685ac7b4687c0ffdcf80fa48cf
hh-pd <hiromiti999@gmail.com>
2023-04-25 01:24:20 +0900

    [fix] パスワード忘れ画面の修正

--- resources/js/Pages/Auth/ForgotPassword.tsx
+++ resources/js/Pages/Auth/ForgotPassword.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
 import Button from '@/Components/Button'
 import Guest from '@/Layouts/Guest'
 import Input from '@/Components/Input'
+import Link from '@/Components/Link'
 import ValidationErrors from '@/Components/ValidationErrors'
-import { useForm } from '@inertiajs/inertia-react'
-import { Head } from '@inertiajs/react'
+import { Head, useForm } from '@inertiajs/react'
 
 interface Props {
     status: string
 }
 
 export default function ForgotPassword({ status }: Props) {
-    const { data, setData, post, processing, errors } = useForm({
+    const { data, setData, post, errors } = useForm({
         email: '',
     })
 
@@ -29,17 +28,11 @@ export default function ForgotPassword({ status }: Props) {
         <Guest>
             <Head title="パスワードリセット" />
 
-            <div className="mb-4 text-sm text-gray-500 leading-normal">
-                メールアドレスを入力してください。パスワードリセット用のリンクを送ります。
+            <div className="mb-4 sm:text-sm text-xs text-center text-gray-500 leading-normal">
+                メールアドレスを入力してください。
+                <br />
+                パスワードリセット用のリンクを送ります。
             </div>
-
-            {status && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    {status}
-                </div>
-            )}
-
-            <ValidationErrors errors={errors} />
 
             <form onSubmit={submit}>
                 <Input
@@ -49,15 +42,22 @@ export default function ForgotPassword({ status }: Props) {
                     className="mt-1 block w-full"
                     isFocused={true}
                     handleChange={onHandleChange}
+                    validation={errors.email}
                 />
 
-                <div className="flex items-center justify-end mt-4">
-                    <Button
-                        className="ml-4 bg-gray-900"
-                        processing={processing}
-                    >
-                        パスワードのリセットリンクを送る
-                    </Button>
+                {status && (
+                    <div className="mt-4 font-medium text-sm text-green-600">
+                        {status}
+                    </div>
+                )}
+
+                <ValidationErrors errors={errors} />
+
+                <div className="flex items-center justify-center mt-4">
+                    <Link href="login" textColor="text-white" underline={false}>
+                        <Button className="bg-gray-900">戻る</Button>
+                    </Link>
+                    <Button className="ml-4 bg-gray-900">送信</Button>
                 </div>
             </form>
         </Guest>
--- resources/js/Pages/Auth/ResetPassword.tsx
+++ resources/js/Pages/Auth/ResetPassword.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from 'react'
 import Button from '@/Components/Button'
 import Guest from '@/Layouts/Guest'
 import Input from '@/Components/Input'
 import Label from '@/Components/Label'
 import ValidationErrors from '@/Components/ValidationErrors'
-import { useForm } from '@inertiajs/inertia-react'
-import { Head } from '@inertiajs/react'
+import { Head, useForm } from '@inertiajs/react'
 
 interface Props {
     token: string
@@ -15,18 +13,12 @@ interface Props {
 type Name = 'email' | 'password' | 'password_confirmation' | 'token'
 
 export default function ResetPassword({ token, email }: Props) {
-    const { data, setData, post, processing, errors, reset } = useForm({
+    const { data, setData, post, errors, reset } = useForm({
         token: token,
         email: email,
         password: '',
         password_confirmation: '',
     })
-
-    useEffect(() => {
-        return () => {
-            reset('password', 'password_confirmation')
-        }
-    }, [])
 
     const onHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setData(event.target.name as Name, event.target.value)
@@ -35,18 +27,17 @@ export default function ResetPassword({ token, email }: Props) {
     const submit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        post(route('password.update'))
+        post(route('password.store'))
+        reset('password', 'password_confirmation')
     }
 
     return (
         <Guest>
             <Head title="パスワード変更" />
 
-            <ValidationErrors errors={errors} />
-
             <form onSubmit={submit}>
                 <div>
-                    <Label forInput="email" value="Email" />
+                    <Label forInput="email" value="メールアドレス" />
 
                     <Input
                         type="email"
@@ -55,27 +46,31 @@ export default function ResetPassword({ token, email }: Props) {
                         className="mt-1 block w-full"
                         autoComplete="username"
                         handleChange={onHandleChange}
+                        validation={errors.email}
+                        disabled={true}
                     />
                 </div>
 
                 <div className="mt-4">
-                    <Label forInput="password" value="Password" />
+                    <Label forInput="password" value="新規パスワード" />
 
                     <Input
                         type="password"
                         name="password"
                         value={data.password}
                         className="mt-1 block w-full"
+                        placeholder="8文字以上の半角英数字"
                         autoComplete="new-password"
                         isFocused={true}
                         handleChange={onHandleChange}
+                        validation={errors.password}
                     />
                 </div>
 
                 <div className="mt-4">
                     <Label
                         forInput="password_confirmation"
-                        value="Confirm Password"
+                        value="新規パスワード(確認用)"
                     />
 
                     <Input
@@ -85,16 +80,14 @@ export default function ResetPassword({ token, email }: Props) {
                         className="mt-1 block w-full"
                         autoComplete="new-password"
                         handleChange={onHandleChange}
+                        validation={errors.password_confirmation}
                     />
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Button
-                        className="ml-4 bg-gray-900"
-                        processing={processing}
-                    >
-                        パスワードを変更する
-                    </Button>
+                <ValidationErrors errors={errors} />
+
+                <div className="flex items-center justify-center mt-8 mb-3">
+                    <Button className="block w-40">変更</Button>
                 </div>
             </form>
         </Guest>
